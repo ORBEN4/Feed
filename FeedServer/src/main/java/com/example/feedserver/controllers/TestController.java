8f1f075ba134a10da4ca0819e0fdee7e032c4567
ORBEN4 <orbenshloosh@gmail.com>
2022-01-11 17:59:10 +0200

    tested new features

--- FeedServer/src/main/java/com/example/feedserver/controllers/TestController.java
+++ FeedServer/src/main/java/com/example/feedserver/controllers/TestController.java
@@ -2,18 +2,21 @@ package com.example.feedserver.controllers;
 
 import com.example.feedserver.FeedServerApplication;
 import com.example.feedserver.constant.Controllers;
-import com.example.feedserver.registration.memorymanager.MySqlMemoryManager;
+import com.example.feedserver.helpers.ConfigurationManager;
+import com.example.feedserver.memorymanagers.MySqlMemoryManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 //import org.springframework.security.access.prepost.PreAuthorize;
 
 @RestController
 @RequestMapping(Controllers.TEST)
 public class TestController extends RouterController {
+//    @Autowired
+//    private Environment environment;
 
     @Override
     @GetMapping(path = {"", "/menu"})
@@ -34,12 +37,12 @@ public class TestController extends RouterController {
 
         MySqlMemoryManager mySqlMemoryManager = new MySqlMemoryManager();
 //        return mySqlMemoryManager.test("select * from users.genders");
-        return mySqlMemoryManager.test(rawQuery);
+        return mySqlMemoryManager.testReadQuery(rawQuery);
 //        return new MySqlMemoryManager().test(rawQuery);
     }
 
     @GetMapping("kill")
-    public ResponseEntity kill() {
+    public ResponseEntity<String> kill() {
 //        FeedServerApplication.starter.close();
 //        System.out.println("close");
 
@@ -52,7 +55,44 @@ public class TestController extends RouterController {
             }
         }.start();
 
-        return new ResponseEntity<>("<h1>success<h1/>", HttpStatus.OK);
+        return new ResponseEntity<>("<body style= \"background-color:chartreuse\" ><h1><b>success</b></h1></body>", HttpStatus.OK);
+    }
+
+    @GetMapping(path = "getDriverName")
+    public ResponseEntity getDriverName(){
+        try {
+            return new ResponseEntity<>(ConfigurationManager.getInstance().getProperty("feed.micro-services.registration.database.driver.name"), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    @GetMapping(path = "environment")
+    public ResponseEntity environment(){
+        try {
+            return new ResponseEntity<>(ConfigurationManager.getInstance().getProperty("feed.micro-services.registration.database.driver.name"), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(path = "environmentInput")
+    public ResponseEntity environmentInput(@RequestHeader("key") String key){
+        try {
+            return new ResponseEntity<>(ConfigurationManager.getInstance().getProperty(key), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(path = "environmentIsInitialized")
+    public ResponseEntity environmentIsInitialized(){
+        try {
+            return new ResponseEntity<>(ConfigurationManager.getInstance().isInitialized(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
 
