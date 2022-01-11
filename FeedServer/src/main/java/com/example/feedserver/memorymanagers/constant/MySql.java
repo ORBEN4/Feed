f55e02bd8141365108ed8799c6b79842e5712a97
ORBEN4 <orbenshloosh@gmail.com>
2022-01-11 18:01:40 +0200

    created constant to support db connection properties

---
+++ FeedServer/src/main/java/com/example/feedserver/memorymanagers/constant/MySql.java
@@ -0,0 +1,8 @@
+package com.example.feedserver.memorymanagers.constant;
+
+public class MySql {
+    public static final String DRIVER = "feed.micro-services.registration.database.driver.name";
+    public static final String CONNECTION_STRING = "feed.micro-services.registration.database.connection-string";
+    public static final String USERNAME = "feed.micro-services.registration.database.username";
+    public static final String PASSWORD = "feed.micro-services.registration.database.password";
+}
