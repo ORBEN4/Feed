04b45de1493c9091194b2637b89893826a2f6b03
ORBEN4 <orbenshloosh@gmail.com>
2022-01-24 17:53:15 +0200

    added test router

--- FeedServer/src/main/java/com/example/feedserver/controllers/RouterController.java
+++ FeedServer/src/main/java/com/example/feedserver/controllers/RouterController.java
@@ -1,6 +1,8 @@
 package com.example.feedserver.controllers;
 
 import com.example.feedserver.constant.Controllers;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -56,5 +58,8 @@ public class RouterController {
 //        return stringBuilder.toString();
 //    }
 
-
+    @GetMapping(path = {"/Hello", "/hello"})
+    public ResponseEntity<String> hello() {
+        return new ResponseEntity<>("hello", HttpStatus.OK);
+    }
 }
